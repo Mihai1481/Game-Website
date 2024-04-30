653f7551cdcc1156a151fca53d418521fe0f2a98
Mihai1481 <mihaieduard1481@gmail.com>
2024-04-30 16:50:52 +0300

    Building Platform Selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,13 @@
+import useData from "./use.Data"
+
+
+interface Platform{
+    id:number,
+    name:string,
+    slug:string
+
+}
+
+const usePlatforms = () => useData<Platform>('platforms/lists/parents')
+
+export default usePlatforms
